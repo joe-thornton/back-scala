08a126074591df6c0668442dc8ce6bb06cbb31a6
Joe Thornton <thornton.jp@gmail.com>
2022-08-07 11:03:58 +0100

    statement generator prints line for single transaction

--- src/main/scala/StatementGenerator.scala
+++ src/main/scala/StatementGenerator.scala
@@ -1,4 +1,5 @@
 import scala.collection.mutable
+import com.github.nscala_time.time.Imports._
 
 trait StatementBase {
   def print(ledger: scala.collection.mutable.Set[Transaction]): String
@@ -6,7 +7,19 @@ trait StatementBase {
 
 object StatementGenerator extends StatementBase {
   def print(ledger: scala.collection.mutable.Set[Transaction]): String = {
-    "Amount      | Date        | Balance     "
+    val header: String = "Amount, Date, Balance\n"
+    val transactionLines: String = printLedger(ledger)
+    val text = header.concat(transactionLines)
+    text
+  }
+
+  private[this] def printLedger(ledger: scala.collection.mutable.Set[Transaction]): String = {
+    if (ledger.isEmpty) ""
+    else f"${ledger.head.amount}%.2f | ${dateTimeString(ledger.head.dateAndTime)} | ${ledger.head.amount}%.2f\n"
+  }
+
+  private[this] def dateTimeString(dateAndTime: LocalDateTime): String = {
+    dateAndTime.toString("dd/MM/yy")
   }
 }
 
--- src/test/scala/StatementGeneratorSpec.scala
+++ src/test/scala/StatementGeneratorSpec.scala
@@ -8,16 +8,18 @@ class StatementGeneratorSpec extends AnyWordSpec with Matchers with MockFactory 
   "A statement" should {
     "print only the top row where the ledger is empty" in {
       val mockLedger = scala.collection.mutable.Set.empty[Transaction]
-      StatementGenerator.print(mockLedger) shouldEqual "Amount      | Date        | Balance     "
+      StatementGenerator.print(mockLedger) shouldEqual "Amount, Date, Balance\n"
     }
   }
 
-/*  "A statement" should {
-    "print only the top row where the ledger is empty" in {
-      val transactionDate = new LocalDateTime("2022-07-27T11:39:45.618")
-      val mockLedger = scala.collection.mutable.Set(Transaction(10, transactionDate))
+  "A statement" should {
+    "print an amount, date and balance for single transaction" in {
+      val mockTransaction = Transaction(100, new LocalDateTime("2022-07-27T11:39:45.618"))
+      val mockLedger = scala.collection.mutable.Set(mockTransaction)
+      val expectedString = "Amount, Date, Balance\n100.00 | 27/07/22 | 100.00\n"
+      StatementGenerator.print(mockLedger) shouldEqual expectedString
     }
-  }*/
+  }
 
 }
 
